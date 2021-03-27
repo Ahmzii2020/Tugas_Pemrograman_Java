ab4220891a64e1f5ca5f326f767dbbf3ddf0afd4
Ahmad Fauzi <81090647+Ahmzii2020@users.noreply.github.com>
2021-03-27 09:23:05 +0700

    Update SepedaGunungBeraksi2.java

--- Belajar-TP2/SepedaGunungBeraksi2.java
+++ Belajar-TP2/SepedaGunungBeraksi2.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package BelajarTP1;
+package BelajarTP2;
 /* Eksekusi Program */
     public class SepedaGunungBeraksi2 {
        public static void main(String[] args) {
