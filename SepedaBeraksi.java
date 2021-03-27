08fc31748344a4b49b9ffc0e126ea9a1d408f1d5
Ahmad Fauzi <81090647+Ahmzii2020@users.noreply.github.com>
2021-03-27 07:39:29 +0700

    Add files via upload

---
+++ SepedaBeraksi.java
@@ -0,0 +1,16 @@
+// Nama     : Ahmad Fauzi
+// NIM      : 11200930000020
+// Prodi    : Sistem Informasi
+// Tanggal  : 18 Maret 2021   
+// Tugas Pertemuan 2 Karakteristik Pemrograman Berorientasi Objek
+package BelajarTP1;
+    public class SepedaBeraksi{
+                    public static void main(String[] args) {
+                            Sepeda sepedaku = new Sepeda();
+
+                            sepedaku.setGir(1);
+                            /* Variabel dapat di ubah ataupun tidak */
+                            sepedaku.gir = 3; 			
+                            System.out.println("Gir saat ini:" + sepedaku.getGir());
+                    }
+    }
