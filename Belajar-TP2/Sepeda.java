4c33fe43a34bb329dd7fcd2988daa018fba999d6
Ahmad Fauzi <81090647+Ahmzii2020@users.noreply.github.com>
2021-03-27 09:21:46 +0700

    Update Sepeda.java

--- Belajar-TP2/Sepeda.java
+++ Belajar-TP2/Sepeda.java
@@ -3,7 +3,7 @@
 // Prodi    : Sistem Informasi
 // Tanggal  : 18 Maret 2021   
 // Latihan Pertemuan 2 Karakteristik Pemrograman Berorientasi Objek
-package BelajarTP1;
+package BelajarTP2;
 /* Pembentukan Method */
     public class Sepeda{
            int gir;
