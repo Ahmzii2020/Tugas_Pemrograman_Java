012bc2e0c4a4c21fa34c1636ca8a7cc27ee4aad8
Ahmad Fauzi <81090647+Ahmzii2020@users.noreply.github.com>
2021-04-22 09:35:11 +0700

    Add files via upload

---
+++ UTS/testPackage.java
@@ -0,0 +1,16 @@
+// Nama     : Ahmad Fauzi
+// NIM      : 11200930000020
+// Kelas    : 2A Sistem Informasi
+// Tanggal  : 22 April 2021
+// Ujian Tengah Semester (UTS) Pemrograman Lanjutan
+package com.iniPackage;
+/**
+ *
+ * @author Ahmad Fauzi
+ */
+public class testPackage {
+    public void test (){
+        System.out.println("Aku Panggil Kamu testPackage !");
+    }
+}
+
