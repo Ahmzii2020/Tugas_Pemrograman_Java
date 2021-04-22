f8df1c9087056681167f1457676e65ed619fd4d6
Ahmad Fauzi <81090647+Ahmzii2020@users.noreply.github.com>
2021-04-22 09:35:42 +0700

    Add files via upload

---
+++ UTS/callPackage.java
@@ -0,0 +1,22 @@
+// Nama     : Ahmad Fauzi
+// NIM      : 11200930000020
+// Kelas    : 2A Sistem Informasi
+// Tanggal  : 22 April 2021
+// Ujian Tengah Semester (UTS) Pemrograman Lanjutan
+package com.panggilPackage;
+
+/**
+ *
+ * @author Ahmad Fauzi
+ */
+import com.iniPackage.*;
+public class callPackage {
+
+
+    public static void  main(String []args  ) {
+
+        testPackage panggildia = new testPackage();
+
+        panggildia.test();
+    }
+}
