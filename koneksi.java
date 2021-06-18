159f6c3f91d46d9a7d428ed00ba9233027627d8e
Ahmad Fauzi <81090647+Ahmzii2020@users.noreply.github.com>
2021-06-18 13:49:56 +0700

    Add files via upload

---
+++ koneksi.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package koneksi;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+/**
+ *
+ * @author User
+ */
+public class koneksi {
+    private Connection koneksi;
+    
+    public Connection connect(){
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            System.out.println("Berhasil Koneksi");
+        }   catch (ClassNotFoundException ex){
+            System.out.println ("Gagal Koneksi"+ex);
+        }
+        
+        String url ="jdbc:mysql://localhost:3306/db_projek";
+        try {
+            koneksi = DriverManager.getConnection(url,"root","");
+            System.out.println("Berhasil Koneksi Database");
+        }   catch (SQLException ex){
+            System.out.println("Gagal Koneksi Database"+ex);
+        }
+        return koneksi;
+    }
+}
